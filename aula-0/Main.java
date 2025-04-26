367b0ce45f287d3318a8de594a3767c7d0e93d58
Ryan18281 <andre.raky1605@gmail.com>
2025-04-26 11:22:44 -0300

    Update Main.java
    primeiro codigo

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
+
+
+
+
+
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+	    
+ char caractere = 'a';
+	   int idade = 15;
+	    boolean tomeicoffe = true;
+	    
+	    Scanner inicio = new Scanner (System.in);
+	    
+	    
+	    
+	    System.out.print("digite o valor da laranja");
+int laranja= inicio.nextInt();
+
+System.out.print("digite o valor da uva");
+int uva= inicio.nextInt();
+	    
+	    
+	    
+	    
+	    
+	   
+	    
+	   System.out.println("resultado da soma = "+(laranja + uva) );
+	    System.out.println("matheus gay");
 		System.out.println("Hello World");
 	}
 }
