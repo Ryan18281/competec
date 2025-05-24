5cf3ba7ab77e4ed1c96f0636c5ee9734f007fd57
Ryan18281 <andre.raky1605@gmail.com>
2025-05-24 11:30:51 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-	}
+		Scanner sa = new Scanner (System.in);
+		
+	    int x = 0;
+	    String palavra = sa.nextLine();
+		
+		while(x < 5) {
+		    System.out.println(palavra);
+		   x++;
+		}	
+	  
+	 
+	int f = 1;
+	  int soma = 0;
+	   while (f <= 100) {
+	     soma += f;     
+	     f++;
+	   }  
+	   
+	   System.out.println("valor total final:" +soma);
+	   
+	   int tabuada = sa.nextInt();
+	   for (int s = 1; s <= 10; s++) {
+	       System.out.println(s*tabuada);  
+	        
+	   }*/
+	   
+	  
+	   
+   }
 }
