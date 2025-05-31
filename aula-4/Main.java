7c3ae517c1cbb7dd09d710f79667574f439ce4dd
Ryan18281 <andre.raky1605@gmail.com>
2025-05-31 11:11:57 -0300

    Update Main.java

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+ Scanner sa = new Scanner (System.in);
+ 
+ /*
+ for (int n = 0; n <=100; n++){
+     System.out.print(10 * n);
+     System.out.print(" ");
+     
+ }*/
+/*nt numeros []= {7,2,6}; 
+ System.out.print("numeros: ");
+numeros [0]=7;
+numeros [1]=2;
+ numeros [2]=6;
+ int soma = numeros [0]+numeros [1]+numeros [2];
+System.out.print(soma);*/
+
+System.out.print("digite o  numero inteiro:");
+int meros = sa.nextInt(); 
+int arranjo []= new int [meros];
+int valores [ ] = new int [meros];
+for(int i = 0; i < mero;i++){
+ meros[i]= Scanner.nextInt();
+ System.out.print(meros[i] + " ");
+} 
+    
 	}
 }
