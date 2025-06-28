1e96fb8b02e749c3a67a2fae0b9ef3a775ec5b49
Ryan18281 <andre.raky1605@gmail.com>
2025-06-28 11:28:43 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+    Scanner sa = new Scanner (System.in);
+	
+/*
+    System.out.print("digite seu nome:");
+   
+    
+    String ba = sa.nextLine();
+    System.out.println(ba);
+ 
+  int tamanho = ba.length();
+  System.out.print("ola," +ba+ ", seu nome tem " +tamanho+ " caracteres ");
+ */
+ /*
+  System.out.print("Qual a palavra secreta?:");
+  String palavra = sa.nextLine();
+  String secreta ="BATATA";
+ 
+ boolean resultado = (palavra.equalsIgnoreCase(secreta));
+ if (resultado == true){
+   System.out.println("parabens meu fi acertou");
+ }else {
+   System.out.println("ai não meu mano voce errou");
+	}	*/
+ 
 	}
 }
